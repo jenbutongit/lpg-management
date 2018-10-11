a3716d91791669ad83d6828d3b8b870ba8a7630f
Jakub Kozlowski <jakubko@kainos.com>
2018-10-11 11:17:20 +0100

    LPFG-735: course service tests

--- test/unit/lib/courseServiceTest.ts
+++ test/unit/lib/courseServiceTest.ts
@@ -1,4 +1,5 @@
 import {describe} from 'mocha'
+import {Audience} from '../../../src/learning-catalogue/model/audience'
 import {CourseService} from '../../../src/lib/courseService'
 import {Course} from '../../../src/learning-catalogue/model/course'
 import {Event} from '../../../src/learning-catalogue/model/event'
@@ -108,18 +109,70 @@ describe('CourseService tests', () => {
 		)
 	})
 
-	describe('#getAllEventsOnCourse', () => {
-		it('', () => {
-			const course = new Course()
-			const module1 = new FaceToFaceModule()
-			module1.type = Module.Type.FACE_TO_FACE
-			module1.events = [<Event>{id: '1'}]
-			const module2 = new FaceToFaceModule()
-			module2.type = Module.Type.FACE_TO_FACE
-			module2.events = [<Event>{id: '2'}, <Event>{id: '3'}]
-			course.modules = [module1, module2]
+	describe('helper function for displaying audiences on front-end', () => {
+		let course: Course
+		const event1Id = 'event-id-1'
+		const event2Id = 'event-id-2'
+		const event3Id = 'event-id-3'
+		const audience1Id = 'audience-id-1'
+		const audience2Id = 'audience-id-2'
+		let event1: Event
+		let event2: Event
+		const module1Id = 'module-id-1'
+		const module2Id = 'module-id-2'
 
-			expect(courseService.getAllEventsOnCourse(course)).to.be.deep.equal([{id: '1'}, {id: '2'}, {id: '3'}])
+		beforeEach(() => {
+			course = new Course()
+			const module1 = new FaceToFaceModule()
+			module1.id = module1Id
+			module1.type = Module.Type.FACE_TO_FACE
+			event1 = <Event>{id: event1Id}
+			module1.events = [event1]
+			const module2 = new FaceToFaceModule()
+			module2.id = module2Id
+			module2.type = Module.Type.FACE_TO_FACE
+			event2 = <Event>{id: event2Id}
+			const event3 = <Event>{id: event3Id}
+			module2.events = [event2, event3]
+			course.modules = [module1, module2]
+			const audience1 = new Audience()
+			audience1.type = Audience.Type.PRIVATE_COURSE
+			audience1.id = audience1Id
+			audience1.eventId = event1Id
+			const audience2 = new Audience()
+			audience2.type = Audience.Type.PRIVATE_COURSE
+			audience2.id = audience2Id
+			audience2.eventId = event2Id
+			course.audiences = [audience1, audience2]
+		})
+
+		describe('#getAllEventsOnCourse', () => {
+			it('should return an array of events on all course modules', () => {
+				expect(courseService.getAllEventsOnCourse(course)).to.be.deep.equal([
+					{id: event1Id},
+					{id: event2Id},
+					{id: event3Id},
+				])
+			})
+		})
+
+		describe('#getAudienceIdToEventMapping', () => {
+			it('should return a map from an audience id to the audience private course event', () => {
+				const expectedMap: any = {}
+				expectedMap[audience1Id] = event1
+				expectedMap[audience2Id] = event2
+				expect(courseService.getAudienceIdToEventMapping(course)).to.be.deep.equal(expectedMap)
+			})
+		})
+
+		describe('#getEventIdToModuleIdMapping', () => {
+			it('should return a map from private course event id to its corresponding module id ', () => {
+				const expectedMap: any = {}
+				expectedMap[event1Id] = module1Id
+				expectedMap[event2Id] = module2Id
+				expectedMap[event3Id] = module2Id
+				expect(courseService.getEventIdToModuleIdMapping(course)).to.be.deep.equal(expectedMap)
+			})
 		})
 	})
 })
