ebc9bcbd1897bf7df87edad2f7ec7271dc36cbd7
Michael Jones <michaelj@kainos.com>
2018-10-19 15:23:01 +0100

    added learner provider template

--- src/controllers/module/faceToFaceModuleController.ts
+++ src/controllers/module/faceToFaceModuleController.ts
@@ -1,8 +1,8 @@
-import {LearningCatalogue} from '../../learning-catalogue'
-import {Validator} from '../../learning-catalogue/validator/validator'
-import {ModuleFactory} from '../../learning-catalogue/model/factory/moduleFactory'
-import {Request, Response, Router} from 'express'
-import {Module} from '../../learning-catalogue/model/module'
+import { LearningCatalogue } from '../../learning-catalogue'
+import { Validator } from '../../learning-catalogue/validator/validator'
+import { ModuleFactory } from '../../learning-catalogue/model/factory/moduleFactory'
+import { Request, Response, Router } from 'express'
+import { Module } from '../../learning-catalogue/model/module'
 
 export class FaceToFaceModuleController {
 	learningCatalogue: LearningCatalogue
@@ -35,13 +35,14 @@ export class FaceToFaceModuleController {
 			}
 		})
 
-		this.router.get('/content-management/courses/:courseId/module-face-to-face/:moduleId?', this.getModule())
-		this.router.post('/content-management/courses/:courseId/module-face-to-face/:moduleId?', this.setModule())
+		this.router.get('/content-management/courses/:courseId/module-classroom/:moduleId?', this.getModule())
+		this.router.post('/content-management/courses/:courseId/module-classroom/:moduleId?', this.setModule())
+		this.router.get('/content-management/courses/:courseId/module-classroom/add-learning-provider', this.getLearnerProvider())
 	}
 
 	public getModule() {
 		return async (request: Request, response: Response) => {
-			response.render('page/course/module/module-face-to-face')
+			response.render('page/course/module/module-classroom')
 		}
 	}
 
@@ -58,13 +59,25 @@ export class FaceToFaceModuleController {
 			const module = await this.moduleFactory.create(data)
 
 			if (errors.size) {
-				request.session!.sessionFlash = {errors: errors, module: module}
-				return response.redirect(`/content-management/courses/${course.id}/module-face-to-face`)
+				request.session!.sessionFlash = { errors: errors, module: module }
+				return response.redirect(`/content-management/courses/${course.id}/module-classroom`)
 			}
 
 			await this.learningCatalogue.createModule(course.id, module)
 
-			return response.redirect(`/content-management/courses/${course.id}/preview`)
+			return response.redirect(`/content-management/courses/${course.id}/module-classroom/add-learning-provider`)
+		}
+	}
+
+	public getLearnerProvider() {
+		return async (request: Request, response: Response) => {
+			response.render('page/learning-provider/add-learning-provider-to-course')
+		}
+	}
+
+	public setLearnerProvider() {
+		return async (request: Request, response: Response) => {
+			response.render('page/learning-provider/add-learning-provider-policies')
 		}
 	}
 }
