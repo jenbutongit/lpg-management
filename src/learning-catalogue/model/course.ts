ad71abc19dee7732397e2b3fc35291eaad8155c5
Rob Leney <roble@kainos.com>
2018-10-24 10:50:28 +0100

    LPFG-891: Reinstated default 'Draft' status to course

--- src/learning-catalogue/model/course.ts
+++ src/learning-catalogue/model/course.ts
@@ -44,7 +44,7 @@ export class Course {
 		groups: ['all', 'status'],
 		message: 'course.validation.status.invalid',
 	})
-	status: Status
+	status: Status = Status.DRAFT
 
 	getCost() {
 		return this.modules.map(module => module.cost).reduce((acc: number, moduleCost) => acc + (moduleCost || 0), 0)
