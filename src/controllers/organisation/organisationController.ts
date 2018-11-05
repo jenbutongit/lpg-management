ba990f94554ae588edb2cc1c078393ae5a3c9ecb
Michael Jones <michaelj@kainos.com>
2018-11-05 11:49:23 +0000

    added organisations overview template

--- src/controllers/organisation/organisationController.ts
+++ src/controllers/organisation/organisationController.ts
@@ -13,9 +13,17 @@ export class organisationController {
 
     /* istanbul ignore next */
     private setRouterPaths() {
-
+        this.router.get('/content-management/organisations', this.getOrganisations())
         this.router.get('/content-management/add-organisation', this.addOrganisation())
 
+    }
+
+    public getOrganisations() {
+
+        return async (request: Request, response: Response) => {
+
+            response.render('page/organisation/organisations')
+        }
     }
 
     public addOrganisation() {
