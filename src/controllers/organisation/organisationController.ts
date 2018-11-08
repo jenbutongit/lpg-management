a6dfeb9a2eeeafc498bf15bab2c89aa3107da8c6
Michael Jones <michaelj@kainos.com>
2018-11-08 17:02:36 +0000

    remove org template created

--- src/controllers/organisation/organisationController.ts
+++ src/controllers/organisation/organisationController.ts
@@ -1,47 +1,42 @@
-import { Request, Response, Router } from 'express'
+import {Request, Response, Router} from 'express'
 export class organisationController {
-    router: Router
+	router: Router
 
-    constructor(
+	constructor() {
+		this.router = Router()
+		this.setRouterPaths()
+	}
 
-    ) {
+	/* istanbul ignore next */
+	private setRouterPaths() {
+		this.router.get('/content-management/organisations', this.getOrganisations())
+		this.router.get('/content-management/add-organisation', this.addOrganisation())
 
-        this.router = Router()
-        this.setRouterPaths()
-    }
+		this.router.get('/content-management/organisation-overview', this.organisationView())
+		this.router.get('/content-management/delete-organisation', this.deleteOrganisation())
+	}
 
-    /* istanbul ignore next */
-    private setRouterPaths() {
-        this.router.get('/content-management/organisations', this.getOrganisations())
-        this.router.get('/content-management/add-organisation', this.addOrganisation())
+	public getOrganisations() {
+		return async (request: Request, response: Response) => {
+			response.render('page/organisation/organisations')
+		}
+	}
 
-        this.router.get('/content-management/organisation-overview', this.organisationView())
+	public addOrganisation() {
+		return async (request: Request, response: Response) => {
+			response.render('page/organisation/add-organisation')
+		}
+	}
 
-    }
+	public organisationView() {
+		return async (request: Request, response: Response) => {
+			response.render('page/organisation/organisation-overview')
+		}
+	}
 
-    public getOrganisations() {
-
-        return async (request: Request, response: Response) => {
-
-            response.render('page/organisation/organisations')
-        }
-    }
-
-    public addOrganisation() {
-
-        return async (request: Request, response: Response) => {
-
-            response.render('page/organisation/add-organisation')
-        }
-    }
-
-    public organisationView() {
-
-        return async (request: Request, response: Response) => {
-
-            response.render('page/organisation/organisation-overview')
-        }
-    }
-
-
+	public deleteOrganisation() {
+		return async (request: Request, response: Response) => {
+			response.render('page/organisation/delete-organisation')
+		}
+	}
 }
