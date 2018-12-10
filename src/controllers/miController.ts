4e8bc12ed86119a35bc0dafaefac23bb566be389
Michael Jones <michaelj@kainos.com>
2018-12-10 15:43:08 +0000

    added all report templates

--- src/controllers/miController.ts
+++ src/controllers/miController.ts
@@ -10,9 +10,37 @@ export class MiController {
 
 	private configureRouterPaths() {
 		this.router.get('/reporting', this.getReports())
+		this.router.get('/reporting/learner-record', this.getLearnerRecordReport())
+		this.router.get('/reporting/ratings', this.getRatingsReport())
+		this.router.get('/reporting/classroom', this.getClassroomReport())
+		this.router.get('/reporting/professions', this.getProfessionsReport())
 	}
 
 	getReports() {
+		return async (request: Request, response: Response) => {
+			response.render('page/mi')
+		}
+	}
+
+	getLearnerRecordReport() {
+		return async (request: Request, response: Response) => {
+			response.render('page/reports/')
+		}
+	}
+
+	getRatingsReport() {
+		return async (request: Request, response: Response) => {
+			response.render('page/mi')
+		}
+	}
+
+	getClassroomReport() {
+		return async (request: Request, response: Response) => {
+			response.render('page/mi')
+		}
+	}
+
+	getProfessionsReport() {
 		return async (request: Request, response: Response) => {
 			response.render('page/mi')
 		}
