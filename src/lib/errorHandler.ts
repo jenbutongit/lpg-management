78626d3a66d76c22347772a2f236cb7c70dcd9a4
Peter Taylor <petertaylor@Malekith.local>
2019-02-25 10:13:14 +0000

    Redirect to sign out on 401

--- src/lib/errorHandler.ts
+++ src/lib/errorHandler.ts
@@ -1,11 +1,16 @@
 import {NextFunction, Request, Response} from 'express'
 import * as log4js from 'log4js'
+import * as config from '../config'
 
 const logger = log4js.getLogger('controllers/home')
 
 export async function handleError(error: any, request: Request, response: Response, next: NextFunction) {
 	try {
 		logger.error('Error handling request for', request.method, request.url, request.body, '\n', error.stack)
+
+		if (error.response && error.response.status == 401) {
+			return response.redirect(`${config.AUTHENTICATION.callbackUrl}/sign-out`)
+		}
 
 		if (error.response && error.response.status == 403) {
 			const errors = {fields: {fields: ['errors.actionNotAuthorised'], size: 1}}
