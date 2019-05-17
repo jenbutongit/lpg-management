f337337ee88bca0038db068176cce6548f9cdbd3
Yuksel Kurtbas <yukselk@kainos.com>
2019-05-17 16:32:10 +0100

    correct the form

--- src/controllers/skillsController.ts
+++ src/controllers/skillsController.ts
@@ -2,74 +2,83 @@ import {NextFunction, Request, Response, Router} from 'express'
 import {CsrsService} from "../csrs/service/csrsService";
 import * as csvtojson from "csvtojson"
 
+class Choice {
+	value: string
+
+	constructor(val: string) {
+		this.value = val
+	}
+}
+
+class Question {
+	type: string
+	value: string
+	learningName: string
+	learningReference: string
+	choices: Choice[]
+	answers: Choice[]
+
+	constructor(type: string, val: string, learnName: string, learnRef: string, choices: Choice[], answers: Choice[]) {
+		this.answers = answers
+		this.choices = choices
+		this.learningName = learnName
+		this.learningReference = learnRef
+		this.type = type
+		this.value = val
+	}
+}
+
 export class SkillsController {
-    csrsService: CsrsService
-    router: Router
+	csrsService: CsrsService
+	router: Router
 
-    constructor(
-        csrsService: CsrsService
-    ) {
-        this.router = Router()
-        this.configureRouterPaths()
-        this.csrsService = csrsService
-    }
+	constructor(
+		csrsService: CsrsService
+	) {
+		this.router = Router()
+		this.configureRouterPaths()
+		this.csrsService = csrsService
+	}
 
-    private configureRouterPaths() {
-        this.router.get('/content-management/skills', this.getSkills())
-        this.router.post('/content-management/skills', this.uploadAndProcess())
-    }
+	private configureRouterPaths() {
+		this.router.get('/content-management/skills', this.getSkills())
+		this.router.post('/content-management/skills', this.uploadAndProcess())
+	}
 
-    getSkills() {
-        return async (req: Request, res: Response) => {
-            const areasOfWork = await this.csrsService.getAreasOfWork()
+	getSkills() {
+		return async (req: Request, res: Response) => {
+			const areasOfWork = await this.csrsService.getAreasOfWork()
+			res.render('page/skills/skills', areasOfWork)
+		}
+	}
 
-            res.render('page/skills/skills', areasOfWork)
-        }
-    }
+	uploadAndProcess() {
+		return async (req: Request, res: Response, next: NextFunction) =>
 
-    uploadAndProcess() {
-        return async (req: Request, res: Response, next: NextFunction) => {
-
-            // Getting CSV from file uploader, and converting into JSON
+			// Getting CSV from file uploader, and converting into JSON
 			// @ts-ignore
-            csvtojson().fromString(req.files.file.data.toString('utf-8')).then((questions: any) => {
-            	console.log("JSON parsed from CSV: ", questions)
+			csvtojson().fromString(req.files.file.data.toString('utf-8')).then((questions: any) => {
+				console.log("JSON parsed from CSV: ", questions)
 
-				// Creating and populating array of 'Yes' answers for all questions
-				let answers : string[] = []
-				let i : number = 0;
+				const opts = ['A', 'B', 'C', 'D', 'E']
+				let questionToSend: Question[] = []
 
-            	// For each question in JSON:
-				questions.forEach((question : any) => {
-					i++
-					let questionNumber : string = "QUESTION #" + i.toString()
-					answers.push(questionNumber)
-
-					// Check if answer exists in A,B,D,C or E
-					if(question['ANSWER A']){
-						answers.push(question['ANSWER A'])
-					}
-					if(question['ANSWER B']){
-						answers.push(question['ANSWER B'])
-					}
-					if(question['ANSWER C']){
-						answers.push(question['ANSWER C'])
-					}
-					if(question['ANSWER D']){
-						answers.push(question['ANSWER D'])
-					}
-					if(question['ANSWER E']){
-						answers.push(question['ANSWER E'])
-					}
+				questions.forEach((question: any) => {
+					let choices: Choice[] = [], answers: Choice[] = [];
+					opts.forEach((o: string) => {
+						if (question['CHOICE '+ o]) {
+							choices.push(new Choice(question['CHOICE '+ o]))
+						}
+						if (question['ANSWER '+ o] === 'YES') {
+							answers.push(new Choice(question['ANSWER '+ o]))
+						}
+					})
+					questionToSend.push(
+						new Question(question.type, question.QUESTION, question['LEARNING NAME'], question['LEARNING REFERENCE'], choices, answers)
+					)
 				})
-				console.log("Array of 'Yes' answers: ", answers)
-            })
 
-			//TODO: restructure JSON to match endpoint JSON
-			//TODO: verify the record matches what we expect maybe?
-			//TODO: display error on UI
-			// TODO: now send the array to the API
-			// TODO: send success to the UI
-        }
-    }
+				console.log(questionToSend)
+			})
+	}
 }
