6e28fe3a25f1d1832f838011947e8e593c925260
Lauren Taylor <l.taylor@kainos.com>
2019-05-23 17:09:16 +0100

    Error handler for non-CSV files added to file uploader. Success confirmation banner added.

--- src/controllers/skillsController.ts
+++ src/controllers/skillsController.ts
@@ -24,16 +24,20 @@ class Question {
 	value: string
 	learningName: string
 	learningReference: string
+	theme: string
+	why: string
 	choices: Choice[]
 	answers: Choice[]
 
-	constructor(type: string, val: string, learnName: string, learnRef: string, choices: Choice[], answers: Choice[]) {
+	constructor(type: string, val: string, learnName: string, learnRef: string, theme: string, why: string, choices: Choice[], answers: Choice[]) {
 		this.answers = answers
 		this.choices = choices
 		this.learningName = learnName
 		this.learningReference = learnRef
 		this.type = type
 		this.value = val
+		this.theme = theme
+		this.why = why
 	}
 }
 
@@ -56,60 +60,83 @@ export class SkillsController {
 
 	getSkills() {
 		return async (req: Request, res: Response) => {
+			let uploadedFileCSV: boolean = true
+			req.session!.sessionFlash = {uploadedFileCSV}
+
 			const areasOfWork = await this.csrsService.getAreasOfWork()
-			res.render('page/skills/skills', areasOfWork)
+			req.session!.save(() => {
+				res.render('page/skills/skills', areasOfWork)
+			})
 		}
 	}
 
 	uploadAndProcess() {
 		return async (req: Request, res: Response, next: NextFunction) => {
+			let uploadedFileCSV: boolean = true
+
 			// @ts-ignore
-			await csvtojson().fromString(req.files.file.data.toString('utf-8')).then(async (questions: any) => {
+			const uploadedFile = req.files.file
+			let fileExtension = uploadedFile.name.split('.')
+			let fileType = fileExtension[fileExtension.length - 1]
 
-				const opts = ['A', 'B', 'C', 'D', 'E']
-				let questionToSend: Question[] = []
+			console.log("fileType: ", fileType)
+			if (fileType.toLowerCase() !== "csv") {
+				uploadedFileCSV = false
 
-				questions.forEach((question: any) => {
-
-					let choices: Choice[] = [], answers: Choice[] = [];
-					opts.forEach((o: string) => {
-						if (question['CHOICE ' + o]) {
-							choices.push(new Choice(question['CHOICE ' + o]))
-						}
-						if (question['ANSWER ' + o] === 'YES') {
-							answers.push(new Choice(question['ANSWER ' + o]))
-						}
-					})
-					questionToSend.push(
-						new Question(question.TYPE, question.QUESTION, question['LEARNING NAME'], question['LEARNING REFERENCE'], choices, answers)
-					)
+				req.session!.sessionFlash = {uploadedFileCSV}
+				req.session!.save(() => {
+					res.redirect('/content-management/skills')
 				})
+			} else {
 
-				const policyProfession = new Profession()
+				// @ts-ignore
+				await csvtojson().fromString(req.files.file.data.toString('utf-8')).then(async (questions: any) => {
 
-				await this.csrsService.getAreasOfWork()
-					.then(professions => {
-						professions.forEach((profession: Profession) => {
-							if (profession.name.toLowerCase() === "analysis") {
-								let professionHref = profession.href.split('/')
-								let professionId = professionHref[professionHref.length - 1]
-								policyProfession.id = parseInt(professionId, 0)
+					const opts = ['A', 'B', 'C', 'D', 'E']
+					let questionToSend: Question[] = []
+
+					questions.forEach((question: any) => {
+
+						let choices: Choice[] = [], answers: Choice[] = [];
+						opts.forEach((o: string) => {
+							if (question['CHOICE ' + o]) {
+								choices.push(new Choice(question['CHOICE ' + o]))
+							}
+							if (question['ANSWER ' + o] === 'YES') {
+								answers.push(new Choice(question['ANSWER ' + o]))
 							}
 						})
+						questionToSend.push(
+							new Question(question.TYPE, question.QUESTION, question['LEARNING NAME'], question['LEARNING REFERENCE'], question['THEME'], question['WHY'], choices, answers)
+						)
+					})
+
+					const policyProfession = new Profession()
+
+					await this.csrsService.getAreasOfWork()
+						.then(professions => {
+							professions.forEach((profession: Profession) => {
+								if (profession.name.toLowerCase() === "analysis") {
+									let professionHref = profession.href.split('/')
+									let professionId = professionHref[professionHref.length - 1]
+									policyProfession.id = parseInt(professionId, 0)
+								}
+							})
+						}).catch(error => {
+							next(error)
+						})
+
+					const quiz = new Quiz()
+					quiz.profession = policyProfession
+					quiz.questions = questionToSend
+
+					await this.csrsService.postSkills(quiz).then(() => {
+						res.render('page/skills/success')
 					}).catch(error => {
 						next(error)
 					})
-
-				const quiz = new Quiz()
-				quiz.profession = policyProfession
-				quiz.questions = questionToSend
-
-				await this.csrsService.postSkills(quiz).then(() => {
-					res.render('page/skills/success')
-				}).catch(error => {
-					next(error)
 				})
-			})
+			}
 		}
 	}
 }
