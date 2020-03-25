aeffe5479bb566c66b3a07083b3cc31a3a30ef07
Manglesh Jain <manglesh.a.jain@capgemini.com>
2020-03-25 11:04:29 +0000

    Code fix for lc-359, report for mandatory courses

--- src/controllers/reportingController.ts
+++ src/controllers/reportingController.ts
@@ -31,7 +31,8 @@ export class ReportingController {
 	private configureRouterPaths() {
 		this.router.get('/reporting', this.getReports())
 		this.router.post('/reporting/booking-information', this.generateReportBookingInformation())
-		this.router.post('/reporting/learner-record', this.generateReportLearnerRecord())
+		this.router.post('/reporting/learner-record/module', this.generateReportLearnerRecordForModule())
+		this.router.post('/reporting/learner-record/mandatorycourses', this.generateReportForMandatoryCourses())
 	}
 
 	getReports() {
@@ -82,7 +83,7 @@ export class ReportingController {
 		}
 	}
 
-	generateReportLearnerRecord() {
+	generateReportLearnerRecordForModule() {
 		return async (request: Request, response: Response, next: NextFunction) => {
 			const reportType = 'Learner_record_'
 			const filename = reportType.concat(moment().toISOString())
@@ -105,6 +106,47 @@ export class ReportingController {
 				try {
 					await this.reportService
 						.getReportLearnerRecord(dateRange)
+						.then(report => {
+							response.writeHead(200, {
+								'Content-type': 'text/csv',
+								'Content-disposition': `attachment;filename=${filename}.csv`,
+								'Content-length': report.length,
+							})
+							response.end(Buffer.from(report, 'binary'))
+						})
+						.catch(error => {
+							next(error)
+						})
+				} catch (error) {
+					throw new Error(error)
+				}
+			}
+		}
+	}
+
+	generateReportForMandatoryCourses() {
+		return async (request: Request, response: Response, next: NextFunction) => {
+			const reportType = 'Learner_record_'
+			const filename = reportType.concat(moment().toISOString())
+
+			let data = {
+				...request.body,
+			}
+
+			const dateRangeCommand: DateStartEndCommand = this.dateStartEndCommandFactory.create(data)
+			const dateRange: DateStartEnd = dateRangeCommand.asDateRange()
+
+			const errors = await this.dateStartEndValidator.check(dateRange)
+
+			if (errors.size) {
+				request.session!.sessionFlash = {errors}
+				request.session!.save(() => {
+					response.redirect(`/reporting`)
+				})
+			} else {
+				try {
+					await this.reportService
+						.getReportForMandatoryCourses(dateRange)
 						.then(report => {
 							response.writeHead(200, {
 								'Content-type': 'text/csv',
