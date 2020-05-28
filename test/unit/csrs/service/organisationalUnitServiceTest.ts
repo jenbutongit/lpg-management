5f5eef4904c7b27e7aa153dcf22b2fb0e02ef887
matthewmu <matthewmu@kainos.com>
2020-05-28 11:12:26 +0100

    LC-323: adding test for orgUnitService

--- test/unit/csrs/service/organisationalUnitServiceTest.ts
+++ test/unit/csrs/service/organisationalUnitServiceTest.ts
@@ -7,10 +7,8 @@ import {Csrs} from '../../../../src/csrs'
 import {OrganisationalUnitFactory} from '../../../../src/csrs/model/organisationalUnitFactory'
 import {OrganisationalUnitService} from '../../../../src/csrs/service/organisationalUnitService'
 import {OrganisationalUnit} from '../../../../src/csrs/model/organisationalUnit'
-import {AgencyToken} from '../../../../src/csrs/model/agencyToken'
 import {AgencyTokenCapacityUsedHttpService} from '../../../../src/identity/agencyTokenCapacityUsedHttpService'
-import {AgencyTokenCapacityUsed} from '../../../../src/identity/model/AgencyTokenCapacityUsed'
-import {AgencyDomain} from '../../../../src/csrs/model/agencyDomain'
+import {AgencyToken} from '../../../../src/csrs/model/agencyToken'
 
 chai.use(sinonChai)
 
@@ -28,9 +26,13 @@ describe('OrganisationalUnitService tests', () => {
 	})
 
 	it('should get organisationalUnit data', async () => {
+		const agencyToken: AgencyToken = new AgencyToken()
+		agencyToken.uid = 'uid'
+
 		let uri = '1'
 		const organisationalUnit: OrganisationalUnit = new OrganisationalUnit()
 		organisationalUnit.id = uri
+		organisationalUnit.agencyToken = agencyToken
 
 		const parent: OrganisationalUnit = new OrganisationalUnit()
 		parent.id = '2'
@@ -47,93 +49,22 @@ describe('OrganisationalUnitService tests', () => {
 			.resolves(parent)
 		csrs.getOrganisationalUnit = parentOrg
 
+		const agencyTokenStub = sinon
+			.stub()
+			.withArgs('uid')
+			.resolves(agencyToken)
+		agencyTokenCapacityUsedHttpService.getCapacityUsed = agencyTokenStub
+
 		const getOrganisationalUnit = sinon.stub().returns(organisationalUnit)
 		organisationalUnitFactory.create = getOrganisationalUnit
 
 		const data = {
 			id: '1',
+			agencyToken: agencyToken,
 		}
 
 		const result = await organisationalUnitService.getOrganisationalUnit(uri)
 
 		expect(result).to.eql(data)
-	})
-
-	it('should get organisational unit data with agency token data', async () => {
-		let uri = '1'
-		const organisationalUnit: OrganisationalUnit = new OrganisationalUnit()
-		organisationalUnit.id = uri
-
-		const parent: OrganisationalUnit = new OrganisationalUnit()
-		parent.id = '2'
-
-		const agency: AgencyToken = new AgencyToken()
-		agency.uid = '123'
-		agency.capacity = 200
-		agency.token = 'token978'
-		let ad1: AgencyDomain = new AgencyDomain()
-		ad1.domain = 'domain1'
-		let ad2: AgencyDomain = new AgencyDomain()
-		ad2.domain = 'domain2'
-		let agencyDomains: AgencyDomain[] = [ad1, ad2]
-		agency.agencyDomains = agencyDomains
-
-		const agencyTokenCapacityUsed: AgencyTokenCapacityUsed = new AgencyTokenCapacityUsed()
-		agencyTokenCapacityUsed.capacityUsed = '9'
-
-		organisationalUnit.agencyToken = agency
-
-		// TODO - Ask Matt about mocking issue, the parent breaks it
-		/*const parentOrg = sinon
-			.stub()
-			.withArgs(`${uri}/parent`)
-			.resolves(parent)
-		csrs.getOrganisationalUnit = parentOrg*/
-
-		const organisation = sinon
-			.stub()
-			.withArgs(uri)
-			.resolves(organisationalUnit)
-		csrs.getOrganisationalUnit = organisation
-
-		/*const parentOrg = sinon
-			.stub()
-			.withArgs(`${uri}/parent`)
-			.resolves(parent)
-		csrs.getOrganisationalUnit = parentOrg
-*/
-
-		const capacityUsedResponse = sinon
-			.stub()
-			.withArgs(`${agency.uid}`)
-			.resolves(agencyTokenCapacityUsed)
-		agencyTokenCapacityUsedHttpService.getCapacityUsed = capacityUsedResponse
-
-		const getOrganisationalUnit = sinon.stub().returns(organisationalUnit)
-		organisationalUnitFactory.create = getOrganisationalUnit
-
-		const expectedAgencyTokenData = {
-			agencyDomains: [
-				{
-					domain: 'domain1',
-				},
-				{
-					domain: 'domain2',
-				},
-			],
-			capacity: 200,
-			capacityUsed: '9',
-			token: 'token978',
-			uid: '123',
-		}
-
-		const expectedData = {
-			id: '1',
-			agencyToken: expectedAgencyTokenData,
-		}
-
-		const result = await organisationalUnitService.getOrganisationalUnit(uri)
-
-		expect(result).to.eql(expectedData)
 	})
 })
