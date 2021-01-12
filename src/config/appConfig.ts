39a4b77ac297d557c571752aab08f055bf6874e5
Jain Neeraj <neeraj.jain@cabinetoffice.gov.uk>
2021-01-12 12:09:27 +0000

    LC-742 redis session management

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -4,9 +4,8 @@ export class AppConfig {
 		maxAge: 31536000,
 		secure: false,
 	}
-	public name: string = 'lpg-management'
+	public name: string = 'lpg-ui'
 	public resave: boolean = true
 	public saveUninitialized: boolean = true
 	public secret: string = 'dcOVe-ZW3ul77l23GiQSNbTJtMRio87G2yUOUAk_otcbL3uywfyLMZ9NBmDMuuOt'
-	public path: string = `/tmp/sessions`
 }
