47a0eb6b17230cab283714fcf7da7e1b53f508c6
Jain Neeraj <neeraj.jain@cabinetoffice.gov.uk>
2021-01-18 14:57:58 +0000

    LC-742 lpg-management-redis-session-cache

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -4,7 +4,7 @@ export class AppConfig {
 		maxAge: 31536000,
 		secure: false,
 	}
-	public name: string = 'lpg-ui'
+	public name: string = 'lpg-management'
 	public resave: boolean = true
 	public saveUninitialized: boolean = true
 	public secret: string = 'dcOVe-ZW3ul77l23GiQSNbTJtMRio87G2yUOUAk_otcbL3uywfyLMZ9NBmDMuuOt'
